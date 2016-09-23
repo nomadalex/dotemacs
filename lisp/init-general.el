dd414b48814f2ca24f73fbdc8fd091bd68f12c36
Kun Wang <ifreedom.cn@gmail.com>
2016-09-23 14:15:59 +0800

    Move package columns widen to bootstrap.

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -152,22 +152,5 @@ line."
 
   (add-hook 'ibuffer-hook 'ibuffer-set-up-preferred-filters))
 
-;;; package
-(defun sanityinc/set-tabulated-list-column-width (col-name width)
-  "Set any column with name COL-NAME to the given WIDTH."
-  (when (> width (length col-name))
-    (cl-loop for column across tabulated-list-format
-             when (string= col-name (car column))
-             do (setf (elt column 1) width))))
-
-(defun sanityinc/maybe-widen-package-menu-columns ()
-  "Widen some columns of the package menu table to avoid truncation."
-  (when (boundp 'tabulated-list-format)
-    (sanityinc/set-tabulated-list-column-width "Version" 13)
-    (let ((longest-archive-name (apply 'max (mapcar 'length (mapcar 'car package-archives)))))
-      (sanityinc/set-tabulated-list-column-width "Archive" longest-archive-name))))
-
-(add-hook 'package-menu-mode-hook 'sanityinc/maybe-widen-package-menu-columns)
-
 (provide 'init-general)
 ;;; init-general ends here
