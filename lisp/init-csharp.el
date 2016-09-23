25ab07f511a0de8f0204b65117794bb700dbd2a7
Kun Wang <ifreedom.cn@gmail.com>
2016-09-23 15:14:26 +0800

    Remove unnecessary revert-buffer fix for c#.

--- lisp/init-csharp.el
+++ lisp/init-csharp.el
@@ -2,16 +2,12 @@
   :ensure t
   :mode "\\.cs$"
   :config
-  ;; fix revert-buffer advice
-  ;; @see https://code.google.com/p/csharpmode/issues/detail?id=8
   (add-hook 'csharp-mode-hook (lambda ()
                                 (setq tab-width 4)
                                 (setq c-basic-offset 4)
                                 (c-set-offset 'substatement-open 0)
 
-                                (define-key csharp-mode-map [remap c-indent-line-or-region] 'company-indent-for-tab-command)
-
-                                (require 'flymake))))
+                                (define-key csharp-mode-map [remap c-indent-line-or-region] 'company-indent-for-tab-command))))
 
 (provide 'init-csharp)
 ;;; init-csharp ends here
