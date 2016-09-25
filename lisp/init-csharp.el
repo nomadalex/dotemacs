b658b494b2d6b7618a38786d306e4e0fb3c1a7dd
Kun Wang <ifreedom.cn@gmail.com>
2016-09-25 19:24:53 +0800

    Add omnisharp for C# completion.

--- lisp/init-csharp.el
+++ lisp/init-csharp.el
@@ -1,13 +1,31 @@
+(require 'cl)
+
 (use-package csharp-mode
   :ensure t
   :mode "\\.cs$"
-  :config
-  (add-hook 'csharp-mode-hook (lambda ()
-                                (setq tab-width 4)
-                                (setq c-basic-offset 4)
-                                (c-set-offset 'substatement-open 0)
 
-                                (define-key csharp-mode-map [remap c-indent-line-or-region] 'company-indent-for-tab-command))))
+  :init
+  ;; https://github.com/OmniSharp/homebrew-omnisharp-roslyn
+  ;; note: only omnisharp-roslyn v1.6.7.9 can work with unity3d
+  ;;   https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.6.7.9/omnisharp.tar.gz
+  (use-package omnisharp
+    :ensure t
+    :defer t
+    :config
+    (when *is-a-mac*
+      (setq omnisharp-server-executable-path (executable-find "omnisharp")))
+    (add-to-list 'company-backends 'company-omnisharp))
+
+  :config
+  (define-key csharp-mode-map [remap c-indent-line-or-region] 'company-indent-for-tab-command)
+
+  (defun setup-csharp-mode ()
+    (omnisharp-mode)
+    (setq tab-width 4)
+    (setq c-basic-offset 4)
+    (c-set-offset 'substatement-open 0))
+
+  (add-hook 'csharp-mode-hook 'setup-csharp-mode))
 
 (provide 'init-csharp)
 ;;; init-csharp ends here
