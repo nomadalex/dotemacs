20302ce0d45db7a051487f374fd875a614743cfb
Kun Wang <ifreedom.cn@gmail.com>
2017-11-27 16:37:26 +0800

    Use http instead of https for melpa, https will raise gnutls-error.

--- lisp/init-bootstrap.el
+++ lisp/init-bootstrap.el
@@ -15,7 +15,7 @@
 ; (add-to-list 'package-archives
 ;             '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (setq package-enable-at-startup nil)
 (setup-package)
