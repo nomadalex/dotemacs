f92b84d88e475a6fb24eba693b65fe4a390e902b
Kun Wang <ifreedom.cn@gmail.com>
2017-12-03 23:54:32 +0800

    Modify init-ruby.

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -3,8 +3,26 @@
   :ensure t
   :defer t
   :init
+  (diminish 'robe-mode)
   (add-hook 'ruby-mode-hook 'robe-mode)
   (with-eval-after-load 'company
-    (add-to-list 'company-backends 'company-robe)))
+    (push 'company-robe company-backends)))
+
+(use-package projectile-rails
+  :ensure t
+  :defer t
+  :init
+  (projectile-rails-global-mode))
+
+(use-package rinari
+  :disabled
+  :ensure t
+  :defer t)
+
+(use-package flymake-ruby
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'ruby-mode-hook 'flymake-ruby-load))
 
 (provide 'init-ruby)
