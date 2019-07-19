fee13213e3689e51b981cf1807cbec9c04d6086b
Kun Wang <ifreedom.cn@gmail.com>
2019-07-19 17:58:38 +0800

    temp

--- lisp/init-general-modes.el
+++ lisp/init-general-modes.el
@@ -57,4 +57,28 @@
   :ensure t
   :defer t)
 
+(add-to-list 'auto-mode-alist '("\\.m$" . objc-mode))
+(add-to-list 'auto-mode-alist '("\\.mm$" . objc-mode))
+
+(defun my-objc-mode-hook ()
+  ;; Setup indentation
+  (setq tab-width 4)
+  (c-set-style "java")
+  (c-set-offset 'brace-list-close '-)
+  (c-set-offset 'brace-list-intro '0)
+  (c-set-offset 'arglist-close '0)
+  ; brackets should be at same indentation level as the statements they open
+  (c-set-offset 'substatement-open '0) 
+  (c-set-offset 'inline-open '+)
+  (c-set-offset 'block-open '+)
+  ; all "opens" should be indented by the c-indent-level
+  (c-set-offset 'brace-list-open '+)   
+  ; indent case labels by c-indent-level, too
+  (c-set-offset 'case-label '+)
+
+  ;; Fix override of tab switch keybinding
+  (define-key objc-mode-map (kbd "M-j") nil))
+
+(add-hook 'objc-mode-hook 'my-objc-mode-hook)
+
 (provide 'init-general-modes)
